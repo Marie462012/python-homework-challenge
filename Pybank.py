d1538e9adf29e8e7a0301ac2c36b21940c096fa1
Marie462012 <141286619+Marie462012@users.noreply.github.com>
2023-08-31 14:35:00 -0500

    Updates

--- Pybank.py
+++ Pybank.py
@@ -16,6 +16,8 @@ profits = []
 # Open and read the csv and convert into a list of dictionaries
 with open(file_to_load) as budget_data:
     reader =csv.DictReader(budget_data)
+    
+    for row in reader:
 
 #Track the totals
 
@@ -23,9 +25,39 @@ total_months = total_months + 1
 total_pl = total_pl +int(firstrow[1])
 value =int(first_row[1])
 
-#First Row of Data 
-for row in csvreader
+#Track revenue changes
+revenue_change =int(row["Revenue]") - prev_revenue
+prev_revenue = int(row["Revenue"])
+revenue_change_list = revenue_change_list + [revenue_change]
+month_of_change =month_of_change = [row["Date"]]
 
-#Keeping track of the dates
-dates.append(row[0])
+#Calculate the greatest increase
+if (revenue_change > greatest_increase[1]):
+    greatest_increase[0] = row["Date"]
+    greatest_increase[1] = revenue_change
 
+#Calculate the greatest decrease
+if (revenue_change < greatest_decrease [1]):
+    greatest_decrease[0] = row["Date"]
+    greatest_decrease[1] = revenue_change
+    
+#Calculate the Average Revenue Change
+revenue_avg = sum(revenue_change_list) / len(revenue_change_list)
+  
+#Generate Output Summary
+output = (
+    f"\nFinancialAnalysis\n"
+    f"--------------------\n"
+    f"total_months: {total_months}\n"
+    f"total revenue: ${total revenue}\n"
+    f"Average Revenue Change: ${revenue_avg}\n"
+    f"Greatest Increase in Revenue: {greatest_increase[0]} ($[greatest_increase[1]})\n"
+    f"Greatest Decrease in Revenue: [greatest_decrease[0]} ($[greatest_decrease[1])"\n"
+    
+
+#Print the Output
+print(file_to_output)
+
+#Export the results to text file
+with open(file_to_output, "w") as txt_file:
+    txt_file.write(file_to_output)
